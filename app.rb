ecc7daf4ff87ab7ba6b55c5b5713d1b412bb581d
Felicia-Lager-Lofgren-Nti-Johanneberg <97357777+Felicia-Lager-Lofgren-Nti-Johanneberg@users.noreply.github.com>
2023-02-07 10:43:47 +0100

    kommentarer
    kommentarer kring villka funktioner sidan ska ha

--- app.rb
+++ app.rb
@@ -5,7 +5,6 @@ require 'byebug'
 require 'sqlite3'
 require 'bcrypt'
 
-
 enable :sessions
 
 get('/')  do
@@ -16,48 +15,37 @@ get '/index' do
   slim(:index)
 end
 
-post('/users/new') do   #Ny användare
-    
-  username = params[:username]
-   password = params[:password]
-   password_confirm = params[:password_confirm]
- 
-   if (password == password_confirm)
-     #Lägg till användare
-     password_digest = BCrypt::Password.create(password)
-     db = SQLite3::Database.new('db/rocknmyb.db')
-     db.execute("INSERT INTO users (username,pwdigest) VALUES (?,?)", username, password_digest)
-     redirect('/')
-   else
-     "Fel lösen"
-   end
- end
+# 1 Registrera 
 
-get('/showlogin') do   #loginsida
-   slim(:login)
- end
- 
- post('/login') do
-     username = params[:username]
-     password = params[:password]
-     db = SQLite3::Database.new("db/rocknmyb.db")   ### döp om DB Browser till rock'n'myb
-     db.results_as_hash = true
-     result=db.execute("SELECT * FROM users WHERE username=?",username).first
-     pwdigest = result["pwdigest"]
-     id = result["id"]
-   
-     if BCrypt::Password.new(pwdigest) == password
-       session[:id] = id
-       redirect('/')
-     else
-       "Fel lösen 3"  
-     end
-   end
+# 2 Skapa Login och logga in, ha med validering, authorization och authentication
+
+# 3 Visa vilka albums som finns och skapa eget
+
+get('/albums') do
+  db = SQLite3::Database.new("db/rocknmyb.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM Albums")
+  p result
+  slim(:"albums/index",locals:{albums:result})
+end
+
+get('/albums/new') do 
+  slim(:"albums/new")
+end
+
+post('/albums/new') do
+  title = params[:title]
+  artist_id = params[:artist_id].to_i
+  p "Vi fick in datan #{title} och #{artist_id}"
+  db = SQLite3::Database.new("db/rocknmyb.db")
+  db.execute("INSERT INTO Albums (title, artist_id) VALUES (?,?)",title, artist_id)
+  redirect('/albums')
+end
 
 
-#post('/albums/:id/delete') do     # post som raderar och även kommer ihåg vad du har raderat.
-#  id = params[:id].to_i
-#  db = SQLite3::Database.new("db/chinook-crud.db")
-#  db.execute("DELETE FROM albums WHERE AlbumId = ?",id)
-#  redirect('/albums')
-#end
+
+# 4 Skapa artister och forma band (table:Artists)
+
+# 5 Välj instrument till alla spelare (table:Instrument) => relation Artists
+
+# 6 Välj stad att turnera i (table:Tour) => relation Artists
