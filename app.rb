aa786c5a6dbc0d21f06b61deb30cb07d7f40e1be
Felicia-Lager-Lofgren-Nti-Johanneberg <97357777+Felicia-Lager-Lofgren-Nti-Johanneberg@users.noreply.github.com>
2023-03-05 22:00:55 +0100

    fungerande login och registrering samt försök med edit sida

--- app.rb
+++ app.rb
@@ -21,6 +21,10 @@ end
 
 # 1 Skapa regristrerignen
 
+get('/register') do   #regristrering
+  slim(:register)
+end
+
 post('/users/new') do   #Ny användare
     
   username = params[:username]
@@ -31,7 +35,7 @@ post('/users/new') do   #Ny användare
      #Lägg till användare
      password_digest = BCrypt::Password.create(password)
      db = SQLite3::Database.new('db/rocknmyb.db')
-     db.execute("INSERT INTO users (username,pwdigest) VALUES (?,?)", username, password_digest)
+     db.execute("INSERT INTO User (username,pwdigest) VALUES (?,?)", username, password_digest)
      redirect('/')
    else
      "Fel lösen"
@@ -49,7 +53,7 @@ get('/showlogin') do   #loginsida
      password = params[:password]
      db = SQLite3::Database.new('db/rocknmyb.db')   ### döp om DB Browser till rock'n'myb
      db.results_as_hash = true
-     result=db.execute("SELECT * FROM users WHERE username=?",username).first
+     result=db.execute("SELECT * FROM User WHERE username=?",username).first
      pwdigest = result["pwdigest"]
      id = result["id"]
    
@@ -67,8 +71,9 @@ get('/showlogin') do   #loginsida
 get('/albums') do
   db = SQLite3::Database.new("db/rocknmyb.db")
   db.results_as_hash = true
-  result = db.execute("SELECT * FROM Albums")
-  current_albums = db.execute("SELECT album_id FROM Albums")
+  user = session[:id]
+  result = db.execute("SELECT * FROM albums WHERE user_id = ?", user)
+  current_albums = db.execute("SELECT album_id FROM albums")
   p current_albums
   p result
   slim(:"/albums/index",locals:{albums:result})
@@ -81,20 +86,32 @@ end
 post('/albums/new') do #CREATE ALBUM
   title = params[:title]
   artist_id = params[:artist_id].to_i
+  user = session[:id]
   p "Vi fick in datan #{title} och #{artist_id}"
   db = SQLite3::Database.new("db/rocknmyb.db")
-  db.execute("INSERT INTO Albums (title, artist_id) VALUES (?,?)",title, artist_id)
+  db.execute("INSERT INTO albums (title, artist_id, user_id) VALUES (?,?,?)",title, artist_id, user)
+  
   redirect('/albums')
 end
 
 
-get('/albums/:id/edit') do   #EDIT
-  id = params[:id].to_i
+
+get'/albums/:id' do
   db = SQLite3::Database.new("db/rocknmyb.db")
-  db.results_as_hash = true
-  result = db.execute("SELECT * FROM Albums WHERE album_id = ?",id).first
-  p "result är #{result}"
-  slim(:"/albums/edit",locals:{result:result})
+  @result = db.execute("SELECT * FROM albums WHERE album_id = ?",params[:id]).first
+  p "result är #{@result}"
+  
+  slim(:"albums/edit")
+end
+
+post('/update/:id/') do   #EDIT
+  title = params[:title]
+  artist_id = params[:artist_id].to_i
+  user = session[:id]
+  p "Vi fick in datan #{title} och #{artist_id}"
+  db = SQLite3::Database.new("db/rocknmyb.db")
+  db.execute("INSERT INTO albums (title, artist_id, user_id) VALUES (?,?,?)",title, artist_id, user)
+  
   redirect('/albums')
 end
 
@@ -109,8 +126,13 @@ post('/albums/upload_image') do
   redirect('/albums/upload_image')
  end
 
-  
- 
+# post('albums/:id/delete/')
+#   id = params[:id].to_i
+#   db = SQLite3::Database.new("db/rocknmyb.db")
+#   db.execute("DELETE FROM albums WHERE album_id = ?",id)
+# end
+
+
 # 4 Skapa artister och forma band (table:Artists)
 
 # 5 Välj instrument till alla spelare (table:Instrument) => relation Artists
@@ -137,9 +159,42 @@ end
 
 post('/calculate') do
 
+  @num1 = params[:num1].to_f
+  @num2 = params[:num2].to_f
+  @operator = params[:operator]
+  @result = case @operator
+            when '+' then @num1 + @num2
+            when '-' then @num1 - @num2
+            when '*' then @num1 * @num2
+            when '/' then @num1 / @num2
+            end
+  slim :result
   redirect('/tour')
 
 end
+
+#ELLER 
+
+post '/calculate' do
+  num1 = params[:num1].to_f
+  num2 = params[:num2].to_f
+  operator = params[:operator]
+
+  case operator
+  when 'add'
+    result = num1 + num2
+  when 'subtract'
+    result = num1 - num2
+  when 'multiply'
+    result = num1 * num2
+  when 'divide'
+    result = num1 / num2
+  end
+end
+
+
+
+
 
 get('/tour/show') do
   db = SQLite3::Database.new("db/rocknmyb.db")
--- model.rb
+++ model.rb
@@ -1,2 +1,20 @@
 #def register_user(username, password_digest)
 
+class Db_lore
+
+    def new_user
+
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+end
